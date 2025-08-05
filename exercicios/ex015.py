0c50aac8de7c7ba72dfddbe2c092b47586f366b9
lucas mendes <lucasmndes.dev@gmail.com>
2025-08-05 19:17:51 -0300

    concluindo exercicios 14 e 15

---
+++ exercicios/ex015.py
@@ -0,0 +1,4 @@
+km = float(input(' Digite quantos KMs voce percorreu: '))
+dias = int(input(' Digite a quantidade de dias de alguel: '))
+pagamento = (km * 0.15) + (dias * 60)
+print(f'o total a pagar é {pagamento} reais')
